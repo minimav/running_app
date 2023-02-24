37f4b5bc8d8e0aeffdca53e9501748572f52959a
Andrew Davies <6566948+minimav@users.noreply.github.com>
2023-02-24 21:40:49 +0000

    :technologist: prettier pre-commit (#13)
    * :technologist: add prettier pre-commit
    
    * :rotating_light: satisfy prettier pre-commit

--- static/javascript/auth.js
+++ static/javascript/auth.js
@@ -1,67 +1,80 @@
-
 window.onload = function () {
-  const form = document.getElementById("login-form")
+  const form = document.getElementById("login-form");
   form.onsubmit = login.bind(form);
-}
+};
 
 const login = (event) => {
-  event.preventDefault()
+  event.preventDefault();
 
-  const username = event.target.elements.username.value
-  const password = event.target.elements.password.value
+  const username = event.target.elements.username.value;
+  const password = event.target.elements.password.value;
 
-  authAction({ url: "/login", redirectUrl: "/", username, password })
-}
+  authAction({ url: "/login", redirectUrl: "/", username, password });
+};
 
 const authAction = ({ url, redirectUrl, username, password }) => {
   if (username.length === 0) {
-    populateAndShowModal({ title: "Username error", content: "No username was supplied." })
-    return
+    populateAndShowModal({
+      title: "Username error",
+      content: "No username was supplied.",
+    });
+    return;
   } else if (password.length === 0) {
-    populateAndShowModal({ title: "Password error", content: "No password was supplied." })
-    return
+    populateAndShowModal({
+      title: "Password error",
+      content: "No password was supplied.",
+    });
+    return;
   } else if (password.length < 8) {
-    populateAndShowModal({ title: "Password error", content: "Password must have >= 8 characters." })
-    return
+    populateAndShowModal({
+      title: "Password error",
+      content: "Password must have >= 8 characters.",
+    });
+    return;
   }
 
-  const formData = new FormData()
-  formData.append("username", username)
-  formData.append("password", password)
+  const formData = new FormData();
+  formData.append("username", username);
+  formData.append("password", password);
 
   const payload = {
     method: "POST",
-    body: formData
-  }
+    body: formData,
+  };
 
   fetch(url, payload)
-    .then(response => {
+    .then((response) => {
       if (!response.ok) {
-        return Promise.reject()
+        return Promise.reject();
       }
     })
-    .then(_ => {
-      window.location.href = redirectUrl
+    .then((_) => {
+      window.location.href = redirectUrl;
     })
-    .catch(_ => {
-      populateAndShowModal({ title: "Error", content: "Login failed" })
-    })
-}
+    .catch((_) => {
+      populateAndShowModal({ title: "Error", content: "Login failed" });
+    });
+};
 
 const register = () => {
-  const username = document.getElementById("username-input").value
-  const password = document.getElementById("password-input").value
-  authAction({ url: "/register", redirectUrl: "/login?register=True", username, password })
-}
+  const username = document.getElementById("username-input").value;
+  const password = document.getElementById("password-input").value;
+  authAction({
+    url: "/register",
+    redirectUrl: "/login?register=True",
+    username,
+    password,
+  });
+};
 
 document.addEventListener("DOMContentLoaded", function (event) {
   const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
-  })
+  });
   if (params.register) {
     populateAndShowModal({
       title: "Registration success",
-      content: "User successfully registered, please log in."
-    })
+      content: "User successfully registered, please log in.",
+    });
   }
-})
+});
--- static/javascript/geometry.js
+++ static/javascript/geometry.js
@@ -5,24 +5,24 @@
  * @param {boolean} isMiles If we are using miles, else km.
  */
 const haversineDistanceMetres = ([lat1, lng1], [lat2, lng2]) => {
-    const toRadian = angle => (Math.PI / 180) * angle;
-    const distance = (a, b) => (Math.PI / 180) * (a - b);
-    const RADIUS_OF_EARTH_IN_KM = 6371;
+  const toRadian = (angle) => (Math.PI / 180) * angle;
+  const distance = (a, b) => (Math.PI / 180) * (a - b);
+  const RADIUS_OF_EARTH_IN_KM = 6371;
 
-    const dLat = distance(lat2, lat1);
-    const dLon = distance(lng2, lng1);
+  const dLat = distance(lat2, lat1);
+  const dLon = distance(lng2, lng1);
 
-    lat1 = toRadian(lat1);
-    lat2 = toRadian(lat2);
+  lat1 = toRadian(lat1);
+  lat2 = toRadian(lat2);
 
-    // Haversine Formula
-    const a =
-        Math.pow(Math.sin(dLat / 2), 2) +
-        Math.pow(Math.sin(dLon / 2), 2) * Math.cos(lat1) * Math.cos(lat2);
-    const c = 2 * Math.asin(Math.sqrt(a));
+  // Haversine Formula
+  const a =
+    Math.pow(Math.sin(dLat / 2), 2) +
+    Math.pow(Math.sin(dLon / 2), 2) * Math.cos(lat1) * Math.cos(lat2);
+  const c = 2 * Math.asin(Math.sqrt(a));
 
-    return RADIUS_OF_EARTH_IN_KM * c * 1000;
-}
+  return RADIUS_OF_EARTH_IN_KM * c * 1000;
+};
 
 /**
  * Interpolate segment geometry so that points are no more than max distance apart.
@@ -30,74 +30,84 @@ const haversineDistanceMetres = ([lat1, lng1], [lat2, lng2]) => {
  * @param {number} metres Max distance in metres between interpolated points
  */
 const interpolateSegment = (segment, metres) => {
-    const coords = segment["geometry"]["coordinates"]
+  const coords = segment["geometry"]["coordinates"];
 
-    const interpolatedCoords = []
-    coords.forEach(([lng, lat], index) => {
-        interpolatedCoords.push([lng, lat])
+  const interpolatedCoords = [];
+  coords.forEach(([lng, lat], index) => {
+    interpolatedCoords.push([lng, lat]);
 
-        // skip interpolated for final point
-        if (index === coords.length - 1) { return }
+    // skip interpolated for final point
+    if (index === coords.length - 1) {
+      return;
+    }
 
-        // figure out the number of intermediate points required based on distance to the
-        // next point
-        const [nextLng, nextLat] = coords[index + 1]
-        const distanceToNextMetres = haversineDistanceMetres([lat, lng], [nextLat, nextLng])
-        const numIntermediatePoints = Math.floor(distanceToNextMetres / metres)
+    // figure out the number of intermediate points required based on distance to the
+    // next point
+    const [nextLng, nextLat] = coords[index + 1];
+    const distanceToNextMetres = haversineDistanceMetres(
+      [lat, lng],
+      [nextLat, nextLng]
+    );
+    const numIntermediatePoints = Math.floor(distanceToNextMetres / metres);
 
-        // either too short to require interpolation or a rogue multiline geometry
-        if (numIntermediatePoints <= 0 || isNaN(numIntermediatePoints)) { return }
+    // either too short to require interpolation or a rogue multiline geometry
+    if (numIntermediatePoints <= 0 || isNaN(numIntermediatePoints)) {
+      return;
+    }
 
-        // do the interpolated linearly; approximation should be good at the scale we're
-        // working at
-        const latDiff = (nextLat - lat) / (numIntermediatePoints + 1)
-        const lngDiff = (nextLng - lng) / (numIntermediatePoints + 1)
+    // do the interpolated linearly; approximation should be good at the scale we're
+    // working at
+    const latDiff = (nextLat - lat) / (numIntermediatePoints + 1);
+    const lngDiff = (nextLng - lng) / (numIntermediatePoints + 1);
 
-        for (const step of Array(numIntermediatePoints).keys()) {
-            interpolatedCoords.push([lng + (step + 1) * lngDiff, lat + (step + 1) * latDiff])
-        }
-    })
-    segment["geometry"]["coordinates"] = interpolatedCoords
-    return segment
-}
+    for (const step of Array(numIntermediatePoints).keys()) {
+      interpolatedCoords.push([
+        lng + (step + 1) * lngDiff,
+        lat + (step + 1) * latDiff,
+      ]);
+    }
+  });
+  segment["geometry"]["coordinates"] = interpolatedCoords;
+  return segment;
+};
 
 /** Switch the order of cordinates in an array of them e.g. (lng, lat) => (lat, lng) */
-const switchCoords = (coords) => coords.map(([x, y]) => [y, x])
+const switchCoords = (coords) => coords.map(([x, y]) => [y, x]);
 
 /** Bounding box for a set of coordinates. */
 function getBoundingBox(coords) {
-    return {
-        minLng: Math.min(...coords.map(([lng, _]) => lng)),
-        maxLng: Math.max(...coords.map(([lng, _]) => lng)),
-        minLat: Math.min(...coords.map(([_, lat]) => lat)),
-        maxLat: Math.max(...coords.map(([_, lat]) => lat))
-    }
+  return {
+    minLng: Math.min(...coords.map(([lng, _]) => lng)),
+    maxLng: Math.max(...coords.map(([lng, _]) => lng)),
+    minLat: Math.min(...coords.map(([_, lat]) => lat)),
+    maxLat: Math.max(...coords.map(([_, lat]) => lat)),
+  };
 }
 
 /** Jitter a lat-lng randomly. */
 const jitter = ({ lat, lng, jitterProb, maxJitter }) => {
-    if (Math.random() < jitterProb) {
-        return { jitteredLat: lat, jitteredLng: lng }
-    }
-    return {
-        jitteredLat: lat + 2 * (Math.random() - 0.5) * maxJitter,
-        jitteredLng: lng + 2 * (Math.random() - 0.5) * maxJitter
-    }
-}
+  if (Math.random() < jitterProb) {
+    return { jitteredLat: lat, jitteredLng: lng };
+  }
+  return {
+    jitteredLat: lat + 2 * (Math.random() - 0.5) * maxJitter,
+    jitteredLng: lng + 2 * (Math.random() - 0.5) * maxJitter,
+  };
+};
 
 /** Remove points that are too far away from the previous one due to GPS flickering. */
 function removeFlickers(coords, thresholdMetres) {
-    let cleanedCoords = [];
-    coords.forEach(function (point, i) {
-        var comparePoint = coords[i + 1]
-        // use previous point for final point in GPS trace
-        if (comparePoint === undefined) {
-            comparePoint = coords[i - 1]
-        }
-        let distanceMetres = point.distanceTo(comparePoint)
-        if (distanceMetres < thresholdMetres) {
-            cleanedCoords.push(point)
-        }
-    })
-    return cleanedCoords
+  let cleanedCoords = [];
+  coords.forEach(function (point, i) {
+    var comparePoint = coords[i + 1];
+    // use previous point for final point in GPS trace
+    if (comparePoint === undefined) {
+      comparePoint = coords[i - 1];
+    }
+    let distanceMetres = point.distanceTo(comparePoint);
+    if (distanceMetres < thresholdMetres) {
+      cleanedCoords.push(point);
+    }
+  });
+  return cleanedCoords;
 }
